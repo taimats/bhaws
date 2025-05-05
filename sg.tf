c8de1011b497b8ed93c37545700507baf69ffe80
taimats <beo8939@gmail.com>
2025-05-06 01:12:15 +0900

    リソースを適切なものに修正

--- sg.tf
+++ sg.tf
@@ -1,23 +1,51 @@
 # ----------------------
-# ロードバランサー用
+# 踏み台サーバー用
 # ----------------------
-resource "aws_security_group" "elb" {
+resource "aws_security_group" "bastion" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "${var.project}-${var.env}-sg-elb"
+    Name = "${var.project}-${var.env}-sg-bastion"
   }
 }
-resource "aws_security_group_rule" "elb_in_http" {
-  security_group_id = aws_security_group.elb.id
+
+resource "aws_security_group_rule" "bastion_in_ssh" {
+  security_group_id = aws_security_group.bastion.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "bastion_out_for_app" {
+  security_group_id        = aws_security_group.bastion.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 22
+  to_port                  = 22
+  source_security_group_id = aws_security_group.app.id
+}
+
+# ----------------------
+# ロードバランサー用
+# ----------------------
+resource "aws_security_group" "alb" {
+  name   = "${var.project}-${var.env}-sg-alb"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "alb_in_http" {
+  security_group_id = aws_security_group.alb.id
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 80
   to_port           = 80
   cidr_blocks       = ["0.0.0.0/0"]
 }
-resource "aws_security_group_rule" "elb_in_https" {
-  security_group_id = aws_security_group.elb.id
+
+resource "aws_security_group_rule" "alb_in_https" {
+  security_group_id = aws_security_group.alb.id
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 443
@@ -29,63 +57,59 @@ resource "aws_security_group_rule" "elb_in_https" {
 # APサーバー用
 # ----------------------
 resource "aws_security_group" "app" {
+  name   = "${var.project}-${var.env}-sg-app"
   vpc_id = aws_vpc.main.id
+}
 
-  tags = {
-    Name = "${var.project}-${var.env}-sg-app"
-  }
-}
-resource "aws_security_group_rule" "app_in_from_elb" {
+resource "aws_security_group_rule" "app_in_from_bastion" {
   security_group_id        = aws_security_group.app.id
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = 8000
-  to_port                  = 8000
-  cidr_blocks              = ["0.0.0.0/0"]
-  source_security_group_id = aws_security_group.elb.id
+  from_port                = 22
+  to_port                  = 22
+  source_security_group_id = aws_security_group.bastion.id
 }
-resource "aws_security_group_rule" "app_in_from_db" {
+
+resource "aws_security_group_rule" "app_in_from_alb" {
   security_group_id        = aws_security_group.app.id
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = 8000
-  to_port                  = 8000
-  cidr_blocks              = ["0.0.0.0/0"]
-  source_security_group_id = aws_security_group.db.id
+  from_port                = 8080
+  to_port                  = 8080
+  source_security_group_id = aws_security_group.alb.id
 }
-resource "aws_security_group_rule" "app_out" {
+
+resource "aws_security_group_rule" "app_out_https" {
   security_group_id = aws_security_group.app.id
   type              = "egress"
   protocol          = "tcp"
-  from_port         = 3306
-  to_port           = 3306
+  from_port         = 443
+  to_port           = 443
   cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "app_out_for_db" {
+  security_group_id        = aws_security_group.app.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 5432
+  to_port                  = 5432
+  source_security_group_id = aws_security_group.db.id
 }
 
 # ----------------------
 # DBサーバー用
 # ----------------------
 resource "aws_security_group" "db" {
+  name   = "${var.project}-${var.env}-sg-db"
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.project}-${var.env}-sg-db"
-  }
 }
+
 resource "aws_security_group_rule" "db_in_from_app" {
   security_group_id        = aws_security_group.db.id
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = 3306
-  to_port                  = 3306
-  cidr_blocks              = ["0.0.0.0/0"]
+  from_port                = 5432
+  to_port                  = 5432
   source_security_group_id = aws_security_group.app.id
-}
-resource "aws_security_group_rule" "db_out_for_app" {
-  security_group_id = aws_security_group.db.id
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 8000
-  to_port           = 8000
-  cidr_blocks       = ["0.0.0.0/0"]
 }
