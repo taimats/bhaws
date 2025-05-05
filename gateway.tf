e0a1da7cd6c6c220a3da7d17331b5b71cd203c4f
taimats <beo8939@gmail.com>
2025-05-05 21:37:48 +0900

    gatewayの作成

---
+++ gateway.tf
@@ -0,0 +1,48 @@
+# ----------------------
+# Internet Gateway
+# ----------------------
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main
+  
+  tags = {
+    Name = "${var.project}-${var.env}-igw"
+  }
+}
+
+# ----------------------
+# Elastic IP
+# ----------------------
+resource "aws_eip" "nat_3a" {
+  vpc = true
+  
+  tags = {
+    Name = "${var.project}-${var.env}-eip-nat-3a"
+  }
+}
+resource "aws_eip" "nat_3c" {
+  vpc = true
+  
+  tags = {
+    Name = "${var.project}-${var.env}-eip-nat-3c"
+  }
+}
+
+# ----------------------
+# NAT Gateway
+# ----------------------
+resource "aws_nat_gateway" "3a" {
+  subnet_id = aws_subnet.public_3a.id
+  allocation_id = aws_eip.nat_3a.id
+
+  tags = {
+    Name = "${var.project}-${var.env}-nat-3a"
+  }
+}
+resource "aws_nat_gateway" "3c" {
+  subnet_id = aws_subnet.public_3c.id
+  allocation_id = aws_eip.nat_3c.id
+
+  tags = {
+    Name = "${var.project}-${var.env}-nat-3c"
+  }
+}
