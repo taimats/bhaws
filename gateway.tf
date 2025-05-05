5b340247a461eaf99f45ec54b2d5425a3c2be2cb
taimats <beo8939@gmail.com>
2025-05-05 22:05:34 +0900

    fmtの反映

--- gateway.tf
+++ gateway.tf
@@ -3,7 +3,7 @@
 # ----------------------
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main
-  
+
   tags = {
     Name = "${var.project}-${var.env}-igw"
   }
@@ -14,14 +14,14 @@ resource "aws_internet_gateway" "main" {
 # ----------------------
 resource "aws_eip" "nat_3a" {
   vpc = true
-  
+
   tags = {
     Name = "${var.project}-${var.env}-eip-nat-3a"
   }
 }
 resource "aws_eip" "nat_3c" {
   vpc = true
-  
+
   tags = {
     Name = "${var.project}-${var.env}-eip-nat-3c"
   }
@@ -31,7 +31,7 @@ resource "aws_eip" "nat_3c" {
 # NAT Gateway
 # ----------------------
 resource "aws_nat_gateway" "ptivate_3a" {
-  subnet_id = aws_subnet.public_3a.id
+  subnet_id     = aws_subnet.public_3a.id
   allocation_id = aws_eip.nat_3a.id
 
   tags = {
@@ -39,7 +39,7 @@ resource "aws_nat_gateway" "ptivate_3a" {
   }
 }
 resource "aws_nat_gateway" "ptivate_3c" {
-  subnet_id = aws_subnet.public_3c.id
+  subnet_id     = aws_subnet.public_3c.id
   allocation_id = aws_eip.nat_3c.id
 
   tags = {
