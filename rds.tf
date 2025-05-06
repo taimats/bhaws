6d249ee28e846a45f1e1123b4736817a17afd48e
taimats <beo8939@gmail.com>
2025-05-06 16:31:06 +0900

    rdsのリソース作成

---
+++ rds.tf
@@ -0,0 +1,45 @@
+# ----------------------
+# RDSインスタンス
+# ----------------------
+resource "aws_db_instance" "main" {
+  identifier = "${var.project}-${var.env}-postgres"
+
+  engine                = "postgres"
+  engine_version        = "16"
+  instance_class        = "db.t3.micro"
+  allocated_storage     = 2
+  max_allocated_storage = 5
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-3a"
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.db.id]
+  publicly_accessible    = false
+  port                   = 5432
+
+  deletion_protection = false
+  skip_final_snapshot = false
+  apply_immediately   = true
+
+  username = "admin"
+  password = random_password.db_password.result
+  db_name  = "bhapi-db"
+
+  tags = {
+    Name = "${var.project}-${var.env}-postgres"
+  }
+}
+
+# ----------------------
+# RDSの設定関連
+# ----------------------
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.project}-${var.env}-db-subnet-group-postgres"
+  subnet_ids = [aws_subnet.private_3a.id, aws_subnet.private_3c.id]
+}
+
+resource "random_password" "db_password" {
+  length = 16
+}
