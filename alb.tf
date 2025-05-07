491ec51b251163dde65a67df00a8cd674f0fd594
taimats <beo8939@gmail.com>
2025-05-08 02:23:04 +0900

    SSL証明書の指定を追加

--- alb.tf
+++ alb.tf
@@ -23,6 +23,8 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
   protocol          = "HTTPS"
   port              = 443
+  certificate_arn   = aws_acm_certificate.main.arn
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.apps.arn
