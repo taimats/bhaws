0789f09721f0ffa990f142c3054e8e67ea095d1b
taimats <beo8939@gmail.com>
2025-05-08 14:43:25 +0900

    接続に必要な情報を出力

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "app_3a_private_ip" {
+  value     = aws_instance.app_3a.private_ip
+  sensitive = true
+}
+
+output "app_3c_private_ip" {
+  value     = aws_instance.app_3c.private_ip
+  sensitive = true
+}
+
+output "db_endpoint" {
+  value     = aws_db_instance.main.endpoint
+  sensitive = true
+}
+
+output "db_password" {
+  value     = aws_db_instance.main.password
+  sensitive = true
+}
