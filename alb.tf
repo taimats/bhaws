05b3dce3ca5b12322f705d9ed5d5a9f492a80dfe
taimats <beo8939@gmail.com>
2025-05-10 03:19:30 +0900

    ヘルスチェックの変更

--- alb.tf
+++ alb.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "main" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
-  subnets            = [aws_subnet.private_01.id, aws_subnet.private_02.id]
+  subnets            = [aws_subnet.public_01.id, aws_subnet.public_02.id]
 }
 
 resource "aws_lb_listener" "http" {
@@ -31,11 +31,27 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+variable "health_check_path" {
+  type        = string
+  description = "ロードバランサーがヘルスチェックで使用するパス"
+}
+
+variable "health_check_port" {
+  type        = string
+  description = "ロードバランサーがヘルスチェックで使用するポート番号"
+}
+
 resource "aws_lb_target_group" "apps" {
   name     = "${var.project}-${var.env}-lb-target-group-apps"
   protocol = "HTTP"
   port     = 8080
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path     = var.health_check_path
+    port     = var.health_check_port
+    interval = 30
+  }
 }
 
 resource "aws_lb_target_group_attachment" "app_01" {
