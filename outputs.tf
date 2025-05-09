300912a0b6226ef4ae212416dc4b07e0ed67689a
taimats <beo8939@gmail.com>
2025-05-09 20:16:15 +0900

    踏み台サーバーのip出力追加

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "bastion_public_ip" {
+  value     = aws_instance.bastion.public_ip
+  sensitive = true
+}
+
 output "app_01_private_ip" {
   value     = aws_instance.app_01.private_ip
   sensitive = true
